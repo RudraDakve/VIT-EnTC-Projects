66ddc0cd81ef852ab158802cbbf715305fd0f298
RudraDakve <dakverudra2@gmail.com>
2026-08-25 17:09:17 +0530

    Enhance calculator with new operations using cmath library
    Added support for square root and exponentiation operations. Improved user prompts and error handling.

--- calculator.cpp
+++ calculator.cpp
@@ -1,34 +1,49 @@
 #include <iostream>
-#include <bits\stdc++.h>
+#include <cmath>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
     double num1, num2;
     char op;
     char choise;
+    cout << "Welcome to the C++ Calculator!" << endl;
+    cout<< " You can perform the following operations: +, -, *, /, ^, s(square root)" << endl;
     do{
         cout<< "\nEnter first number: ";
         cin>> num1;
-        cout<<"Enter an operator (+, -, *, /): ";
+        cout<<"Enter an operator (+, -, *, /, ^, s): ";
         cin>> op;
+        if(op != 's'){
         cout<<"Enter second number: ";
         cin>>num2;
+        }
         switch(op){
             case '+':
-            cout<< "Result:" << num1 + num2 << endl;
+            cout<< "Result: " << num1 + num2 << endl;
             break;
             case '-':
-            cout<< "Result:" << num1 - num2 << endl;
+            cout<< "Result: " << num1 - num2 << endl;
             break;
             case '*':
-            cout<< "Result:" << num1 * num2 << endl;
+            cout<< "Result: " << num1 * num2 << endl;
             break;
             case '/':
             if(num2 == 0){
                 cout<< "Error! Division by 0 is Mathematically undefined." << endl;
             }else{
-                cout<< "Result:" << num1 - num2 << endl;
+                cout<< "Result: " << num1 / num2 << endl;
             }
+            break;
+            case 's':
+            if(num1 < 0){
+                cout<< "Error! Cannot calculate square root of a negative number." << endl;
+            }else{
+                cout<< "Result: " << sqrt(num1) << endl;
+            }
+            break;
+            case '^':
+            cout<< "Result: " << pow(num1, num2) << endl;
             break;
             default:
             cout << "Invalid operator entered!" << endl;
