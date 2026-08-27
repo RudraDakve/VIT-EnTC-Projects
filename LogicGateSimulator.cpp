3831e7636b1b5e80ff23e5670cd9a626c04fbf46
RudraDakve <dakverudra2@gmail.com>
2026-08-27 12:06:56 +0530

    Add files via upload

--- LogicGateSimulator.cpp
+++ LogicGateSimulator.cpp
@@ -6,13 +6,16 @@ int main(){
     int choice;
     int result;
     cout << "Welcome to C++ Logic Gate Simulator!" << endl;
-    cout << "You can perform any operation on the following logic gates: AND, OR and XOR" <<endl;
+    cout << "You can perform any operation on the following logic gates: AND, OR, NOT, NAND, NOR and XOR" <<endl;
     cout << "Enter any two Binary numbers (0 or 1) to perform the operation: " << endl;
     cin >> input1 >> input2;
     cout << "Enter the operation you want to perform
     1 - for AND
     2 - for OR
-    3 - for XOR" << endl;
+    3 - for NOT
+    4 - for NAND
+    5 - for NOR
+    6 - for XOR" << endl;
     cin >> choice;
     if((input1 != 0 && input1 != 1) && (input2 != 0 && input2 != 1)){
         cout << "Error! : Invalid inputs entered!" << endl;
@@ -28,6 +31,20 @@ int main(){
             cout << "Result of OR operation: " << result << endl;
             break;
         case 3:
+        result = !input1;
+            cout << "Result of NOT operation on first input: " << result << endl;
+        result = !input2;
+            cout << "Result of NOT operation on second input: " << result << endl;
+            break;
+        case 4:
+            result = !(input1 && input2);
+            cout << "Result of NAND operation: " << result << endl;
+            break;
+        case 5:
+            result = !(input1 || input2);
+            cout << "Result of NOR operation: " << result << endl;
+            break;
+        case 6:
             result = input1 ^ input2;
             cout << "Result of XOR operation: " << result << endl;
             break;
